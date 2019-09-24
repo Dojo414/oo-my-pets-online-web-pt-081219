126ef6debfe38c7d95317eeb1bcab568b3a18c54
Donovan Jones <donovanljones94@gmail.com>
2019-09-24 16:07:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,8 @@
 class Owner
   attr_reader :name, :species
   
+  @@all
+  
   def initialize(name, species="human")
   @name = name
   @species = species
@@ -9,4 +11,9 @@ class Owner
   def say_species
     return "I am a #{@species}."
   end
+  
+  def self.all
+    @@all
+  end
+  
 end
